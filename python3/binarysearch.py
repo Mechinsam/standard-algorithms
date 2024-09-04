99f28665e5dd28ebbe3c62eefc1b6809d60aad99
Mechinsam <78374707+Mechinsam@users.noreply.github.com>
2024-09-04 10:04:06 +0100

    fixed slight issue and removed comparisons coz useless

--- python3/binarysearch.py
+++ python3/binarysearch.py
@@ -5,11 +5,9 @@ def BinarySearch(list_ : list, target : int) -> int: # doing 'list_' instead of 
 	"""
 	iMax = len(list_) - 1
 	iMin, iMid = 0, 0 # this sets them both to 0 in one line
-	global comparisons
 
-	while (iMax >= iMin)/2:
+	while (iMax >= iMin):
 		iMid = int((iMax + iMin)/2) # specified int because we dont want decimal places
-		comparisons += 1
 
 		if list_[iMid] == target:
 			return iMid
@@ -24,11 +22,12 @@ if __name__ == "__main__":
 	# example
 	searchArray = [1,3,5,7,8,9,11,34,45]
 	target = 4
-	comparisons = 0
+
+	print(f"array: {searchArray}\ntarget:4\n-------\n")
 	
 	variablePosition = BinarySearch(searchArray, target)
 
 	if variablePosition == None: # we didnt get position returned
 		print("Target not found")
 	else:
-		print(f"Target found in element {variablePosition} of the array\nNumber of comparisons: {comparisons}")
+		print(f"Target found in element {variablePosition} of the array")
